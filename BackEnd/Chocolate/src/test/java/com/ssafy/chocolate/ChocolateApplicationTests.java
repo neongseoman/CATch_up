2c463dffd33ae966d6d9b81e1ac13db304c4d2f5
박근수 <rmstn8426@gmail.com>
2024-01-16 17:19:09 +0900

    backend create

---
+++ BackEnd/Chocolate/src/test/java/com/ssafy/chocolate/ChocolateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.chocolate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocolateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
