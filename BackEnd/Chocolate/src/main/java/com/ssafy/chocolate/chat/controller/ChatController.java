09e488df17b076ff672f7aeab8fd744da573d832
허준호 <nidolight@icloud.com>
2024-01-23 12:59:50 +0900

    feat: chat user name added

--- BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/chat/controller/ChatController.java
+++ BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/chat/controller/ChatController.java
@@ -5,11 +5,16 @@ package com.ssafy.chocolate.chat.controller;//
 
 
 import com.ssafy.chocolate.chat.service.TopicService;
+import com.ssafy.chocolate.user.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -25,6 +30,14 @@ public class ChatController {
         topicService.deleteTopic(topicName);
         return "success";
     }
+    public String getCurrentUsername() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            return ((UserDetails)principal).getUsername();
+        } else {
+            return principal.toString(); // 또는 null 반환
+        }
+    }
     @MessageMapping("/chat.sendToNewTopic") // 클라이언트에서 해당 경로로 메시지를 전송..
     public void sendToNewTopic(@Payload String message) {
         String topicName = message.split("LLLL")[1];
@@ -32,7 +45,14 @@ public class ChatController {
         messagingTemplate.convertAndSend(topicPath, message);
     }
     @GetMapping({"/chatapp"})
-    public String chatApp() {
+    public String chatApp(Model model) {
         return "chatapp.html";
+    }
+
+
+    @GetMapping("/api/userinfo")
+    public ResponseEntity<?> getUserInfo() {
+        String username = getCurrentUsername();
+        return ResponseEntity.ok().body(username);
     }
 }
