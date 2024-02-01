f911fac939d58e661f10cda22427c9dfd31ef503
허준호 <nidolight@icloud.com>
2024-02-01 18:05:55 +0900

    fix: http to https

--- BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/user/controller/DashboardController.java
+++ BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/user/controller/DashboardController.java
@@ -18,7 +18,7 @@ import java.util.Map;
 import java.util.Optional;
 
 
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="https://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class DashboardController {
