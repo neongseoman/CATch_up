297ee5bff028bffea65bb1c35cb9ea95070a60b1
devjun20 <fwccjs@gmail.com>
2024-02-02 14:58:07 +0900

    fix: api 엔드포인트 수정

--- BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/search/controller/SearchController.java
+++ BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/search/controller/SearchController.java
@@ -9,10 +9,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/search")
 public class SearchController {
 
     @Autowired
