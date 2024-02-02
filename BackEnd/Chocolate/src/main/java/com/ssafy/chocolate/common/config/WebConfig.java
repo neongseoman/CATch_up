5421093b045ccb8efcb421e616477e7f163bc654
허준호 <nidolight@icloud.com>
2024-02-02 15:41:50 +0900

    feat: dev, prod seperation

--- BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/common/config/WebConfig.java
+++ BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/common/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.ssafy.chocolate.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,10 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.origins}")
+    private String[] allowedOrigins;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://localhost:3000", "https://i10a105.p.ssafy.io")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true) // 인증 정보 허용
                 .allowedHeaders("*") // 허용할 헤더
