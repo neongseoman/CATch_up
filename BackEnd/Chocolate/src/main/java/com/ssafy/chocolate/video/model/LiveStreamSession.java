4042a95d3b1090f587f86123be557b853d6b1b00
devjun20 <fwccjs@gmail.com>
2024-02-05 11:06:09 +0900

    feat: 특정 유저 스트리밍 기록 보기

---
+++ BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/video/model/LiveStreamSession.java
@@ -0,0 +1,52 @@
+package com.ssafy.chocolate.video.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "live_stream_sessions")
+public class LiveStreamSession {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "stream_no")
+    private Integer streamNo;
+
+    @Column(name = "user_no")
+    private Integer userNo;
+
+    @Column(name = "start_time")
+    private Date startTime;
+
+    @Column(name = "end_time")
+    private Date endTime;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "introduction")
+    private String introduction;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "max_viewer")
+    private Integer maxViewer;
+
+}
---
+++ BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/video/repository/LiveStreamSessionJpaRepository.java
@@ -0,0 +1,11 @@
+package com.ssafy.chocolate.video.repository;
+
+import com.ssafy.chocolate.video.model.LiveStreamSession;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface LiveStreamSessionJpaRepository extends JpaRepository<LiveStreamSession, Integer> {
+    List<LiveStreamSession> findByUserNo(int userNo);
+}
