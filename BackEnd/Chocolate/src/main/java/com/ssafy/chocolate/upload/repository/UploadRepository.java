6ddf9990d60a1aba5eb1a2fc24609a0249cdd26d
devjun20 <fwccjs@gmail.com>
2024-02-06 17:16:12 +0900

    feat: 쇼츠 영상 파일업로드 구현

--- BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/upload/repository/UploadRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.chocolate.upload.repository;
-
-import com.ssafy.chocolate.upload.model.ImageDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UploadRepository extends JpaRepository<ImageDto, Long> {
-}
