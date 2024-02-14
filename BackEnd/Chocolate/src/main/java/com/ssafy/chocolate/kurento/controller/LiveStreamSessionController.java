46e6a25e4306bc258ee4e7e9e85ffbc0818d8848
허준호 <nidolight@icloud.com>
2024-02-14 17:49:21 +0900

    fix: follow api fixed

--- BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/kurento/controller/LiveStreamSessionController.java
+++ BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/kurento/controller/LiveStreamSessionController.java
@@ -6,18 +6,17 @@ import com.ssafy.chocolate.kurento.service.Busking;
 import com.ssafy.chocolate.kurento.service.IceMessageSendService;
 import com.ssafy.chocolate.user.repository.MemberRepository;
 import com.ssafy.chocolate.video.model.LiveStreamSession;
+import com.ssafy.chocolate.video.repository.LiveStreamSessionJpaRepository;
 import com.ssafy.chocolate.video.service.LiveStreamSessionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -25,7 +24,8 @@ public class LiveStreamSessionController {
 
     @Autowired
     private LiveStreamSessionService service;
-
+    @Autowired
+    private LiveStreamSessionJpaRepository liveStreamSessionJpaRepository;
     @Autowired
     private MemberRepository userRepository;
 
@@ -35,8 +35,16 @@ public class LiveStreamSessionController {
 
         LiveStreamSession session = new LiveStreamSession();
         session.setUserNo(userRepository.findUserIdByUsername(email));
-        session.setLatitude(dto.getGeoLocation().getLatitude());
-        session.setLongitude(dto.getGeoLocation().getLongitude());
+
+        if (dto.getGeoLocation() != null) {
+            session.setLatitude(dto.getGeoLocation().getLatitude());
+            session.setLongitude(dto.getGeoLocation().getLongitude());
+
+        } else {
+            session.setLatitude(0.0);
+            session.setLongitude(0.0);
+        }
+
         session.setCategory(dto.getBuskingHashtag());
         session.setIntroduction(dto.getBuskingInfo());
         session.setStartTime(LocalDateTime.now());
@@ -45,4 +53,11 @@ public class LiveStreamSessionController {
         LiveStreamSession savedSession = service.createLiveStreamSession(session);
         return ResponseEntity.ok(savedSession);
     }
+
+    @GetMapping("/user/{userNo}/active")
+    public ResponseEntity<List<LiveStreamSession>> getActiveSessionsByUserNo(@PathVariable Long userNo) {
+        List<LiveStreamSession> sessions = liveStreamSessionJpaRepository.findByUserNoAndEndTimeIsNull(userNo);
+        return ResponseEntity.ok(sessions);
+    }
+
 }
