c02a099ea8a70057517b6cf566840560cff90134
박근수 <rmstn8426@gmail.com>
2024-02-14 19:51:44 +0900

    error logging

--- FrontEnd/src/pages/Watching.jsx
+++ FrontEnd/src/pages/Watching.jsx
@@ -3,7 +3,7 @@ import CustomText from "../components/CustomText";
 import {PCConfig} from "../WebRTC/RTCConfig";
 import * as StompJS from "@stomp/stompjs";
 import * as SockJS from "sockjs-client";
-import {koreaTime} from "../WebRTC/PCEvent";
+import { getKoreaTime} from "../Apis/common";
 import {useRecoilState} from "recoil";
 import {userInfoState} from "../RecoilState/userRecoilState";
 
@@ -27,7 +27,6 @@ const Watching = ({buskerEmail}) => {
 
     useEffect(() => {
         const remoteVideo = document.getElementById("remoteVideo")
-
         pc.onicecandidate = (event) => { //setLocalDescription이 불러옴.
             if (event.candidate) {
                 // console.log("Client Send Ice Candidate : [ " + event.candidate.candidate + " ] ")
@@ -45,56 +44,52 @@ const Watching = ({buskerEmail}) => {
             }
         }
         pc.oniceconnectionstatechange = (event) => {
-            console.log(koreaTime+' - ICE 연결 상태:', pc.iceConnectionState);
+            const currentKoreaTime = getKoreaTime();
+            console.log(currentKoreaTime+' - ICE 연결 상태:', pc.iceConnectionState);
             if (pc.iceConnectionState === 'new'){
-                console.log(koreaTime +' 피어 연결을 시작 합니다. ')
+                console.log(currentKoreaTime +' 피어 연결을 시작 합니다. ')
             }
-            console.log(koreaTime +' ICE 연결 상태:', pc.iceConnectionState);
+            console.log(currentKoreaTime +' ICE 연결 상태:', pc.iceConnectionState);
+            pc.restartIce()
             if (pc.iceConnectionState === 'connected') {
-                console.log(koreaTime +' 피어 간 연결이 성공적으로 수립되었습니다.');
+                console.log(currentKoreaTime +' 피어 간 연결이 성공적으로 수립되었습니다.');
             } else if (pc.iceConnectionState === 'disconnected' || pc.iceConnectionState === 'failed'){
                 if (pc){
                     pc.close()
                 }
                 pcRef.current = new RTCPeerConnection(PCConfig);
 
-                console.log(koreaTime +' 피어 간 연결이 끊어졌거나 실패했습니다.');
+                console.log(currentKoreaTime +' 피어 간 연결이 끊어졌거나 실패했습니다.');
+                pc.restartIce()
             }
         };
         pc.onconnectionstatechange = (event) => { // 데이터 연결 상태 확인
-            console.log(koreaTime+' - 데이터 연결 상태:', pc.connectionState);
-            if (pc.connectionState === 'connected') {
-                console.log(koreaTime +' 데이터 연결이 확립되었습니다.');
-            } else if (pc.connectionState === 'disconnected') {
-                console.log(koreaTime +' 데이터 연결이 끊어졌습니다.');
-            } else if(pc.connectionState === "failed"){
 
+            const currentKoreaTime = getKoreaTime();
+            console.log(currentKoreaTime+' - 데이터 연결 상태:', pc.connectionState);
+            if (pc.connectionState === 'connected') {
+                console.log(currentKoreaTime +' 데이터 연결이 확립되었습니다.');
+            } else if (pc.connectionState === 'disconnected') {
+                console.log(currentKoreaTime +' 데이터 연결이 끊어졌습니다.');
+                pc.restartIce()
+            } else if(pc.connectionState === "failed"){
+                console.log(currentKoreaTime +' 데이터 연결을 실패했습니다.');
+                pc.restartIce()
             }
         };
         pc.ontrack = (event) =>{
             remoteVideo.srcObject = event.streams[0]
         }
         pc.onsignalingstatechange = (event) => {
-            console.log(koreaTime + " signaling 상태가 바뀝니다.")
+            const currentKoreaTime = getKoreaTime();
+
+            console.log(currentKoreaTime + " signaling 상태가 바뀝니다.")
             console.log(pc.signalingState)
         }
         pc.onnegotiationneeded = (event) => {
-            console.log(koreaTime+ " Negotiation을 진행합니다.")
-            makingOffer = true
-        }
+            const currentKoreaTime = getKoreaTime();
+            console.log(currentKoreaTime+ " Negotiation 진행합니다.")
 
-
-        if (typeof WebSocket !== 'function') {
-            client.webSocketFactory = function () {
-                console.log("Stomp error sockjs is running");
-                return new SockJS('https://i10a105.p.ssafy.io/api/signal');
-            };
-        }
-
-        client.onConnect = (frame) => {
-            console.log(frame); // stomp status
-
-            makingOffer = true
             pc.createOffer({
                 offerToReceiveAudio:true,
                 offerToReceiveVideo:true
@@ -118,6 +113,44 @@ const Watching = ({buskerEmail}) => {
                 .catch((error) => {
                     console.log(error)
                 })
+        }
+
+        if (typeof WebSocket !== 'function') {
+            client.webSocketFactory = function () {
+                console.log("Stomp error sockjs is running");
+                return new SockJS('https://i10a105.p.ssafy.io/api/signal');
+            };
+        }
+
+        client.onConnect = (frame) => {
+
+            const currentKoreaTime = getKoreaTime();
+            console.log(frame); // stomp status
+
+            makingOffer = true
+            // pc.createOffer({
+            //     offerToReceiveAudio:true,
+            //     offerToReceiveVideo:true
+            // })
+            //     .then((offer) => {
+            //         console.log("sdp offer created") // sdp status
+            //         pc.setLocalDescription(offer)
+            //             .then((r) => {
+            //                 client.publish({
+            //                     destination: `/app/api/audience/${userId}/offer`,
+            //                     body: JSON.stringify({
+            //                         buskerId,
+            //                         audienceId: userId,
+            //                         offer,
+            //                     })
+            //                 })
+            //                 new RTCPeerConnectionIceEvent("onicecandidate")
+            //                 makingOffer = false
+            //             })
+            //     })
+            //     .catch((error) => {
+            //         console.log(error)
+            //     })
 
             // sdpOffer를 보내고 Answer를 받음
             client.subscribe(`/audience/${userId}/sdpAnswer`, (res) => {
@@ -139,12 +172,16 @@ const Watching = ({buskerEmail}) => {
             })
 
             client.subscribe(`/audience/${userId}/iceCandidate`, (res) => {
+
+                const currentKoreaTime = getKoreaTime();
                 const iceResponse = JSON.parse(res.body);
-                if (iceResponse.id === "iceCandidate") {
-                    console.log(koreaTime + " server send ice \n" + iceResponse.candidate.candidate)
+                if (iceResponse.id === "iceCandidate" && pc.connectionState !== "connected") {
+                    console.log(currentKoreaTime + " server send ice \n" + iceResponse.candidate.candidate)
                     const icecandidate = new RTCIceCandidate(iceResponse.candidate)
                     pc.addIceCandidate(icecandidate)
                         .then()
+                } else if (iceResponse.id === "iceCandidate" && pc.connectionState === "connected"){
+                    pc.restartIce()
                 }
             })
         }
