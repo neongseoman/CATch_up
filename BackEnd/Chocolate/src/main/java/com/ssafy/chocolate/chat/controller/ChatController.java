83e1ff13ea7f2a842c4ffdbaa388ee061f9cd2a4
neongseoman <rmstn8426@gmail.com>
2024-02-17 21:07:58 +0900

    fix : import error fix

--- BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/chat/controller/ChatController.java
+++ BackEnd/Chocolate/src/main/java/com/ssafy/chocolate/chat/controller/ChatController.java
@@ -9,6 +9,7 @@ import com.ssafy.chocolate.user.model.Member;
 import com.ssafy.chocolate.user.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
